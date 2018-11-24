b4d308941044d76f047205d871f253797fcb2484
Raymond Zhang <raymondzhang0126@gmail.com>
2018-11-24 14:56:02 -0800

    Initialization

---
+++ pointer_reference.cpp
@@ -0,0 +1,20 @@
+// Example program
+#include <iostream>
+#include <string>
+#include <typeinfo>
+using namespace std;
+
+int main()
+{
+    int size = 5;
+    int& a = size;
+    int* b = &size;
+    cout << a << endl;
+    cout << typeid(a).name() << endl;
+    cout << b << endl;
+    cout << typeid(b).name() << endl;
+  //string name;
+  //cout << "What is your name? ";
+  //getline (cin, name);
+  //cout << "Hello, " << name << "!\n";
+}
